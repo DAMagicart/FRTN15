c41b9c9c408a404f4214e10f9759a70e44745c4d
trulsmartinsson <62885481+trulsmartinsson@users.noreply.github.com>
2020-04-26 15:41:59 +0200

    Add files via upload

---
+++ solveJacobianVector.m
@@ -0,0 +1,41 @@
+function [J] = solveJacobianVector(x,u,h,Q,R)
+%Try to solve the jacobian J(x,u) = int{||x||^2 + ||u||^2} with fmincon
+%The function will return J = [u1(k), u2(k)] which is the optimal
+%control law for u, Hu time steps forward in time.
+Hp = length(x)/4;           %Prediction Horizon  
+Hu = length(u)/2;           %Control Horizon 
+
+%Jag vet inte värdet på A och B. Dessa fås från diskretiseringen!
+A = eye(4);                %test - 4*4
+B = ones(4,2);             %test - 4*1
+
+%Constraints for the control signal u - Jag vet inte vad dessa ska va.
+Au = eye(2*Hu);
+Bu = ones(2*Hu,1);
+u0 = zeros(2*Hu,1);
+                
+Mp = zeros(Hp*4, Hu*2);
+Np = zeros(4*Hp, 4);
+row = zeros(4, Hu*2);
+b = (h*B);
+a = (1+h*A);
+row(1:4,1:2) = b;
+for i = 1:Hp
+    Mp(4*i-3:4*i,:) = row;
+    Np(4*i-3:4*i,:) = a^i;
+    row = circshift(row',2)';
+    row(1:4,1:2) = (a^i)*b;
+end
+x = reshape(x, [4,Hp]);
+
+Xk = (Mp*u)+(Np*x(:,1));     %Xk -> (Ekv. 16.40, Rolf Johansson)
+Qmat = Q*eye(4*Hp);
+Rmat = zeros(2*Hu, 2*Hu);
+for k = 1:Hu
+    Rmat(k,k) = R;
+end
+
+Jt = @(t)[(Xk'*Qmat*Xk) + [t]'*Rmat*[t]];
+J1 = fmincon(Jt, u0, Au, Bu);
+J = reshape(J1, [2,Hu])';
+end
